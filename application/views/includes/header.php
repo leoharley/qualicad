d31401bb71e1bdbc89ffa728c9a5dc28e12a9c8c
Leonardo <leoharleygoncalves@gmail.com>
2022-02-17 20:46:45 -0300

    ...

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -76,7 +76,7 @@
             <!-- User Account: style can be found in dropdown.less -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="<?php echo base_url(); ?>assets/dist/img/avatar.png" class="user-image" alt="User Image" />
+                <img src="<?php echo base_url(); ?>assets/dist/img/avatar2.png" class="user-image" alt="User Image" />
                 <span class="hidden-xs">
                   <?php echo $name; ?>
                 </span>
@@ -84,7 +84,7 @@
               <ul class="dropdown-menu">
                 <!-- User image -->
                 <li class="user-header">
-                  <img src="<?php echo base_url(); ?>assets/dist/img/avatar.png" class="img-circle" alt="User Image" />
+                  <img src="<?php echo base_url(); ?>assets/dist/img/avatar2.png" class="img-circle" alt="User Image" />
                   <p>
                     <?php echo $name; ?>
                     <small>
