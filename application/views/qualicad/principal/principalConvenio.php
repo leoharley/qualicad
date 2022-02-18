8425a164ff96d181220d3761f13b986f1df2430a
Leonardo <leoharleygoncalves@gmail.com>
2022-02-17 22:16:00 -0300

    ...

--- application/views/qualicad/principal/principalConvenio.php
+++ application/views/qualicad/principal/principalConvenio.php
@@ -2,8 +2,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            <i class="fa fa-users"></i> Kullanıcı Yönetimi
-            <small>Ekle / Düzenle</small>
+            <i class="fa fa-users"></i> Cadastrar Convênio
+            <small>Associar / Adicionar</small>
         </h1>
     </section>
 
@@ -18,7 +18,7 @@
 
                 <div class="box box-primary">
                     <div class="box-header">
-                        <h3 class="box-title">Kullanıcı bilgilerini giriniz</h3>
+                        <h3 class="box-title">Selecione e preencha os campos abaixo</h3>
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
@@ -28,15 +28,17 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="fname">Ad Soyad</label>
-                                        <input type="text" class="form-control required" value="<?php echo set_value('fname'); ?>" id="fname" name="fname" maxlength="128">
+                                        <label for="nomeempresa">Empresa associada</label>
+                                        <select class="form-control required" id="nomeempresa" name="nomeempresa">
+                                            <option value="1">EMPRESA_1</option>
+											<option value="2">EMPRESA_2</option>
+                                        </select>
                                     </div>
-
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="email">Email adresi</label>
-                                        <input type="text" class="form-control required email" id="email" value="<?php echo set_value('email'); ?>" name="email"
+                                        <label for="dsconvenio">Descrição</label>
+                                        <input type="text" class="form-control required email" id="dsconvenio" value="<?php echo set_value('dsconvenio'); ?>" name="dsconvenio"
                                             maxlength="128">
                                     </div>
                                 </div>
@@ -44,44 +46,47 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="password">Şifre</label>
-                                        <input type="password" class="form-control required" id="password" name="password" maxlength="20">
+                                        <label for="cnpjconvenio">CNPJ</label>
+                                        <input type="text" class="form-control required email" id="cnpjconvenio" value="<?php echo set_value('cnpjconvenio'); ?>" name="cnpjconvenio"
+                                            maxlength="13">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="cpassword">Şifre Doğrula</label>
-                                        <input type="password" class="form-control required equalTo" id="cpassword" name="cpassword" maxlength="20">
+                                        <label for="cdconvenioerp">Código</label>
+                                        <input type="text" class="form-control required email" id="cdconvenioerp" value="<?php echo set_value('cdconvenioerp'); ?>" name="cdconvenioerp"
+                                            maxlength="11">
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="mobile">Telefon Numarası</label>
-                                        <input type="text" class="form-control required digits" id="mobile" value="<?php echo set_value('mobile'); ?>" name="mobile"
-                                            maxlength="10">
+                                        <label for="tpconvenio">Tipo</label>
+                                        <select class="form-control required" id="tpconvenio" name="tpconvenio">
+                                            <option value="1">TIPO_CONVENIO_1</option>
+											<option value="2">TIPO_CONVENIO_2</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="role">Yetki</label>
-                                        <select class="form-control required" id="role" name="role">
-                                            <option value="0">Yetki seçiniz</option>
-                                            <?php
-                                            if(!empty($roles))
-                                            {
-                                                foreach ($roles as $rl)
-                                                {
-                                                    ?>
-                                                <option value="<?php echo $rl->roleId ?>" <?php if($rl->roleId == set_value('role')) {echo "selected=selected";} ?>>
-                                                    <?php echo $rl->role ?>
-                                                </option>
-                                                <?php
-                                                }
-                                            }
-                                            ?>
-                                        </select>
+                                        <label for="dtinicioconvenio">Data de ínicio</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtinicioconvenio'); ?>" id="dtinicioconvenio" name="dtinicioconvenio">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="dtvigenciaconvenio">Data de vigência</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtvigenciaconvenio'); ?>" id="dtvigenciaconvenio" name="dtvigenciaconvenio">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="dtcriacao">Data de criação</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtcriacao'); ?>" id="dtcriacao" name="dtcriacao">
                                     </div>
                                 </div>
                             </div>
@@ -89,8 +94,8 @@
                         <!-- /.box-body -->
 
                         <div class="box-footer">
-                            <input type="submit" class="btn btn-primary" value="Gönder" />
-                            <input type="reset" class="btn btn-default" value="Sıfırla" />
+                            <input type="submit" class="btn btn-primary" value="Salvar" />
+                            <input type="reset" class="btn btn-default" value="Limpar" />
                         </div>
                     </form>
                 </div>
