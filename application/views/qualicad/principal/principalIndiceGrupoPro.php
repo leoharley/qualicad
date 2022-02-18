5a81855395af336245935dedae5a30c11f461f6a
Leonardo <leoharleygoncalves@gmail.com>
2022-02-17 23:22:53 -0300

    ...

--- application/views/qualicad/principal/principalIndiceGrupoPro.php
+++ application/views/qualicad/principal/principalIndiceGrupoPro.php
@@ -2,8 +2,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            <i class="fa fa-users"></i> Kullanıcı Yönetimi
-            <small>Ekle / Düzenle</small>
+            <i class="fa fa-users"></i> Cadastrar Índice Grupo
+            <small>Associar / Adicionar</small>
         </h1>
     </section>
 
@@ -18,7 +18,7 @@
 
                 <div class="box box-primary">
                     <div class="box-header">
-                        <h3 class="box-title">Kullanıcı bilgilerini giriniz</h3>
+                        <h3 class="box-title">Selecione e preencha os campos abaixo</h3>
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
@@ -28,69 +28,83 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="fname">Ad Soyad</label>
-                                        <input type="text" class="form-control required" value="<?php echo set_value('fname'); ?>" id="fname" name="fname" maxlength="128">
-                                    </div>
-
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="email">Email adresi</label>
-                                        <input type="text" class="form-control required email" id="email" value="<?php echo set_value('email'); ?>" name="email"
-                                            maxlength="128">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="password">Şifre</label>
-                                        <input type="password" class="form-control required" id="password" name="password" maxlength="20">
+                                        <label for="dsgrupopro">Grupo pro associado</label>
+                                        <select class="form-control required" id="dsgrupopro" name="dsgrupopro">
+                                            <option value="1">GRUPO_PRO_1</option>
+											<option value="2">GRUPO_PRO_2</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="cpassword">Şifre Doğrula</label>
-                                        <input type="password" class="form-control required equalTo" id="cpassword" name="cpassword" maxlength="20">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="mobile">Telefon Numarası</label>
-                                        <input type="text" class="form-control required digits" id="mobile" value="<?php echo set_value('mobile'); ?>" name="mobile"
-                                            maxlength="10">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="role">Yetki</label>
-                                        <select class="form-control required" id="role" name="role">
-                                            <option value="0">Yetki seçiniz</option>
-                                            <?php
-                                            if(!empty($roles))
-                                            {
-                                                foreach ($roles as $rl)
-                                                {
-                                                    ?>
-                                                <option value="<?php echo $rl->roleId ?>" <?php if($rl->roleId == set_value('role')) {echo "selected=selected";} ?>>
-                                                    <?php echo $rl->role ?>
-                                                </option>
-                                                <?php
-                                                }
-                                            }
-                                            ?>
+                                        <label for="dsindice">Índice associado</label>
+                                        <select class="form-control required" id="dsindice" name="dsindice">
+                                            <option value="1">INDICE_1</option>
+											<option value="2">INDICE_2</option>
                                         </select>
                                     </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="dtinivigencia">Data de início da vigência</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtinivigencia'); ?>" id="dtinivigencia" name="dtinivigencia">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="dtfimvigencia">Data de fim da vigência</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtfimvigencia'); ?>" id="dtfimvigencia" name="dtfimvigencia">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="vlindice">Valor do índice</label>
+                                        <input type="text" class="form-control required" value="<?php echo set_value('vlindice'); ?>" id="vlindice" name="vlindice"
+                                        maxlength="11">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="vlm2filme">Valor m2 filme</label>
+                                        <input type="text" class="form-control required" value="<?php echo set_value('vlm2filme'); ?>" id="vlm2filme" name="vlm2filme"
+                                        maxlength="11">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="vlhonorario">Valor honorário</label>
+                                        <input type="text" class="form-control required" value="<?php echo set_value('vlhonorario'); ?>" id="vlhonorario" name="vlhonorario"
+                                        maxlength="11">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="vluco">Valor UCO</label>
+                                        <input type="text" class="form-control required" value="<?php echo set_value('vluco'); ?>" id="vluco" name="vluco"
+                                        maxlength="11">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="dtcriacao">Data de criação</label>
+                                        <input type="date" class="form-control required" value="<?php echo set_value('dtcriacao'); ?>" id="dtcriacao" name="dtcriacao">
+                                    </div>
+                                </div>
+                            </div>  
                         </div>
                         <!-- /.box-body -->
 
                         <div class="box-footer">
-                            <input type="submit" class="btn btn-primary" value="Gönder" />
-                            <input type="reset" class="btn btn-default" value="Sıfırla" />
+                            <input type="submit" class="btn btn-primary" value="Salvar" />
+                            <input type="reset" class="btn btn-default" value="Limpar" />
                         </div>
                     </form>
                 </div>
