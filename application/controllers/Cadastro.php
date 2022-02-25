bcab270c0ef0453cee29207228ef1ffb7d0060ee
Leonardo <leoharleygoncalves@gmail.com>
2022-02-25 00:15:58 -0300

    ...

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -271,7 +271,6 @@ class Cadastro extends BaseController
     function apagaUsuario()
     {
             $IdUsuario = $this->uri->segment(2);
-            var_dump($IdUsuario);exit;
             $infoUsuario = array('Deletado'=>'S','AtualizadoPor'=>$this->vendorId, 'Dt_Atualizacao'=>date('Y-m-d H:i:s'));
             
             $resultado = $this->CadastroModel->apagaUsuario($IdUsuario, $infoUsuario);
