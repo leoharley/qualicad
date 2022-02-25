555073c7a9e20d06908933bd5093a2b1afb6fc46
Leonardo <leoharleygoncalves@gmail.com>
2022-02-24 22:27:49 -0300

    ...

--- application/views/qualicad/cadastro/c_cadastroUsuario.php
+++ application/views/qualicad/cadastro/c_cadastroUsuario.php
@@ -26,8 +26,8 @@ if(!empty($infoUsuario))
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            <i class="fa fa-users"></i> <? ($this->uri->segment(2) == 'adicionar') ? 'Cadastrar Usuários' : 'Editar Usuários' ; ?>
-            <small><? ($this->uri->segment(2) == 'adicionar') ? 'Adicionar' : 'Editar' ; ?></small>
+            <i class="fa fa-users"></i> <?php echo ($this->uri->segment(2) == 'adicionar') ? 'Cadastrar Usuários' : 'Editar Usuários' ; ?>
+            <small><?php echo ($this->uri->segment(2) == 'adicionar') ? 'Adicionar' : 'Editar' ; ?></small>
         </h1>
     </section>
 
