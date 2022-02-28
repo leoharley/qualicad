ed515c6a1296170a3b1424b55ec88d072fc43f36
Leonardo <leoharleygoncalves@gmail.com>
2022-02-28 20:39:19 -0300

    ...

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -23,7 +23,7 @@
     <div class="login-logo">
       <a href="#">
 			<b>QUALICAD</b>
-        <br>PAINEL ADMIN MODELO<?php echo $empresasPerfilUsuario;?></a>
+        <br>PAINEL ADMIN MODELO</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
