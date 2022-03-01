2b8065c8b419036965e030fcd8075bb43329630a
Leonardo <leoharleygoncalves@gmail.com>
2022-02-28 23:54:38 -0300

    ...

--- application/views/qualicad/cadastro/c_cadastroUsuarioEmpresa.php
+++ application/views/qualicad/cadastro/c_cadastroUsuarioEmpresa.php
@@ -21,6 +21,34 @@ if(!empty($infoUsuarioEmpresa))
 
 ?>
 
+<?php $this->load->helper('form'); ?>
+      <div class="row">
+        <div class="col-md-12">
+          <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">', ' <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>'); ?>
+        </div>
+      </div>
+      <?php
+        $this->load->helper('form');
+        $error = $this->session->flashdata('error');
+        if($error)
+        {
+            ?>
+        <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <?php echo $error; ?>
+        </div>
+        <?php }
+        $success = $this->session->flashdata('success');
+        if($success)
+        {
+            ?>
+        <div class="alert alert-success alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <?php echo $success; ?>
+        </div>
+        <?php } ?>
+        
+
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
