a82d9a90731c7c5cc912020e5a48475edb6fdb65
Leonardo <leoharleygoncalves@gmail.com>
2022-02-28 21:52:30 -0300

    ...

--- application/config/constants.php
+++ application/config/constants.php
@@ -87,9 +87,7 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 
 /**** USER DEFINED CONSTANTS **********/
 
-define('ROLE_ADMIN',                            '1');
-define('ROLE_MANAGER',                         	'2');
-define('ROLE_EMPLOYEE',                         '3');
+define('ROLE_ADMIN',                            '0');
 
 define('SEGMENT',								2);
 
