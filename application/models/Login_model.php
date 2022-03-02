b967d3ab78261adef92e2fc04a3f3522bc3d934d
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-03-02 17:12:52 -0300

    ...

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -99,7 +99,7 @@ class Login_model extends CI_Model
         $this->db->where('email', $email);
         $this->db->where('activation_id', $activation_id);
         $query = $this->db->get();
-        var_dump($email);var_dump($activation_id);exit;
+        var_dump($query->num_rows);exit;
         return $query->num_rows;
     }
 
