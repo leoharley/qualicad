e08b26ec59720239d2acc032472ce65ab88f4421
Leonardo <leoharleygoncalves@gmail.com>
2022-03-02 00:13:43 -0300

    ...

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,6 +16,8 @@ class Login extends BaseController
     public function __construct()
     {
         parent::__construct();
+        $ci = get_instance();
+        $ci->load->library('email');
         $this->load->model('login_model');
         $this->load->model('CadastroModel');
     }
