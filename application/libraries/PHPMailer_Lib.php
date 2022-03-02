8e44ad8a0122422c0e50170572d219251c656ed9
Leonardo <leoharleygoncalves@gmail.com>
2022-03-02 08:42:39 -0300

    ...

--- application/libraries/PHPMailer_Lib.php
+++ application/libraries/PHPMailer_Lib.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-class Phpmailer_Lib
+class Phpmailer_lib
 {
 public function __construct(){
     log_message('Debug', 'PHPMailer class is loaded.');
