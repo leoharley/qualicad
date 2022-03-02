bb0c882925767c9fababd429ecdd48a361abe456
Leonardo <leoharleygoncalves@gmail.com>
2022-03-02 00:12:38 -0300

    ...

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session');
+$autoload['libraries'] = array('database', 'session', 'email');
 
 /*
 | -------------------------------------------------------------------
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url', 'file','cias_helper');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array('email');
+$autoload['config'] = array();
 
 /*
 | -------------------------------------------------------------------
