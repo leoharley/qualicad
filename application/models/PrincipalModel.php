5b13102cbc05599ed038f6cc401a080cbe43323b
Leonardo <leoharleygoncalves@gmail.com>
2022-03-02 22:37:19 -0300

    ...

---
+++ application/models/PrincipalModel.php
@@ -0,0 +1,350 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class PrincipalModel extends CI_Model
+{
+    
+// INICIO DAS CONSULTAS NA TELA DE CONVENIO
+    function listaConvenio($id, $searchText = '', $page, $segment)
+    {
+        $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+        $this->db->from('TabUsuario as Usuarios');
+   //     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+        if(!empty($searchText)) {
+            $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                            OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                            OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+            $this->db->where($likeCriteria);
+        }
+        $campos = "((Usuarios.Admin = 'S'
+                    AND Usuarios.CriadoPor = '".$idUser."'))
+                    OR
+                    (Usuarios.Admin = 'N')";
+        $this->db->where($campos);
+
+        $this->db->where('Usuarios.Deletado !=', 'S');
+        $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+        $this->db->where('Usuarios.CriadoPor', $idUser);
+        $this->db->limit($page, $segment);
+        $query = $this->db->get();
+        
+        $result = $query->result();        
+        return $result;
+    }
+
+    function adicionaConvenio($info)
+    {
+        $this->db->trans_start();
+        $this->db->insert('TabUsuario', $infoUsuario);
+        
+        $insert_id = $this->db->insert_id();
+        
+        $this->db->trans_complete();
+        
+        return $insert_id;
+    }
+
+    function editaConvenio($info, $id)
+    {
+        $this->db->where('Id_Usuario', $IdUsuario);
+        $this->db->update('TabUsuario', $infoUsuario);
+        
+        return TRUE;
+    }
+
+    function apagaConvenio($info, $id)
+    {
+        $this->db->where('Id_Usuario', $IdUsuario);
+        $this->db->update('TabUsuario', $infoUsuario);
+        
+        return $this->db->affected_rows();
+    }
+// FIM DAS CONSULTAS NA TELA DE CONVENIO
+
+// INICIO DAS CONSULTAS NA TELA DE PLANO
+function listaPlano($id, $searchText = '', $page, $segment)
+{
+    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+    $this->db->from('TabUsuario as Usuarios');
+//     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+    if(!empty($searchText)) {
+        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $this->db->where($likeCriteria);
+    }
+    $campos = "((Usuarios.Admin = 'S'
+                AND Usuarios.CriadoPor = '".$idUser."'))
+                OR
+                (Usuarios.Admin = 'N')";
+    $this->db->where($campos);
+
+    $this->db->where('Usuarios.Deletado !=', 'S');
+    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->limit($page, $segment);
+    $query = $this->db->get();
+    
+    $result = $query->result();        
+    return $result;
+}
+
+function adicionaPlano($info)
+{
+    $this->db->trans_start();
+    $this->db->insert('TabUsuario', $infoUsuario);
+    
+    $insert_id = $this->db->insert_id();
+    
+    $this->db->trans_complete();
+    
+    return $insert_id;
+}
+
+function editaPlano($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return TRUE;
+}
+
+function apagaPlano($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return $this->db->affected_rows();
+}
+// FIM DAS CONSULTAS NA TELA DE PLANO
+
+// INICIO DAS CONSULTAS NA TELA DE FATURAMENTO
+function listaFaturamento($id, $searchText = '', $page, $segment)
+{
+    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+    $this->db->from('TabUsuario as Usuarios');
+//     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+    if(!empty($searchText)) {
+        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $this->db->where($likeCriteria);
+    }
+    $campos = "((Usuarios.Admin = 'S'
+                AND Usuarios.CriadoPor = '".$idUser."'))
+                OR
+                (Usuarios.Admin = 'N')";
+    $this->db->where($campos);
+
+    $this->db->where('Usuarios.Deletado !=', 'S');
+    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->limit($page, $segment);
+    $query = $this->db->get();
+    
+    $result = $query->result();        
+    return $result;
+}
+
+function adicionaFaturamento($info)
+{
+    $this->db->trans_start();
+    $this->db->insert('TabUsuario', $infoUsuario);
+    
+    $insert_id = $this->db->insert_id();
+    
+    $this->db->trans_complete();
+    
+    return $insert_id;
+}
+
+function editaFaturamento($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return TRUE;
+}
+
+function apagaFaturamento($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return $this->db->affected_rows();
+}
+// FIM DAS CONSULTAS NA TELA DE FATURAMENTO
+
+// INICIO DAS CONSULTAS NA TELA DE FATURAMENTO
+function listaFaturamento($id, $searchText = '', $page, $segment)
+{
+    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+    $this->db->from('TabUsuario as Usuarios');
+//     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+    if(!empty($searchText)) {
+        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $this->db->where($likeCriteria);
+    }
+    $campos = "((Usuarios.Admin = 'S'
+                AND Usuarios.CriadoPor = '".$idUser."'))
+                OR
+                (Usuarios.Admin = 'N')";
+    $this->db->where($campos);
+
+    $this->db->where('Usuarios.Deletado !=', 'S');
+    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->limit($page, $segment);
+    $query = $this->db->get();
+    
+    $result = $query->result();        
+    return $result;
+}
+
+function adicionaFaturamento($info)
+{
+    $this->db->trans_start();
+    $this->db->insert('TabUsuario', $infoUsuario);
+    
+    $insert_id = $this->db->insert_id();
+    
+    $this->db->trans_complete();
+    
+    return $insert_id;
+}
+
+function editaFaturamento($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return TRUE;
+}
+
+function apagaFaturamento($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return $this->db->affected_rows();
+}
+// FIM DAS CONSULTAS NA TELA DE FATURAMENTO
+
+// INICIO DAS CONSULTAS NA TELA DE REGRA
+function listaRegra($id, $searchText = '', $page, $segment)
+{
+    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+    $this->db->from('TabUsuario as Usuarios');
+//     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+    if(!empty($searchText)) {
+        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $this->db->where($likeCriteria);
+    }
+    $campos = "((Usuarios.Admin = 'S'
+                AND Usuarios.CriadoPor = '".$idUser."'))
+                OR
+                (Usuarios.Admin = 'N')";
+    $this->db->where($campos);
+
+    $this->db->where('Usuarios.Deletado !=', 'S');
+    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->limit($page, $segment);
+    $query = $this->db->get();
+    
+    $result = $query->result();        
+    return $result;
+}
+
+function adicionaRegra($info)
+{
+    $this->db->trans_start();
+    $this->db->insert('TabUsuario', $infoUsuario);
+    
+    $insert_id = $this->db->insert_id();
+    
+    $this->db->trans_complete();
+    
+    return $insert_id;
+}
+
+function editaRegra($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return TRUE;
+}
+
+function apagaRegra($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return $this->db->affected_rows();
+}
+// FIM DAS CONSULTAS NA TELA DE REGRA
+
+// INICIO DAS CONSULTAS NA TELA DE INDICE
+function listaIndice($id, $searchText = '', $page, $segment)
+{
+    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
+    $this->db->from('TabUsuario as Usuarios');
+//     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
+    if(!empty($searchText)) {
+        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
+                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
+                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $this->db->where($likeCriteria);
+    }
+    $campos = "((Usuarios.Admin = 'S'
+                AND Usuarios.CriadoPor = '".$idUser."'))
+                OR
+                (Usuarios.Admin = 'N')";
+    $this->db->where($campos);
+
+    $this->db->where('Usuarios.Deletado !=', 'S');
+    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
+    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->limit($page, $segment);
+    $query = $this->db->get();
+    
+    $result = $query->result();        
+    return $result;
+}
+
+function adicionaIndice($info)
+{
+    $this->db->trans_start();
+    $this->db->insert('TabUsuario', $infoUsuario);
+    
+    $insert_id = $this->db->insert_id();
+    
+    $this->db->trans_complete();
+    
+    return $insert_id;
+}
+
+function editaIndice($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return TRUE;
+}
+
+function apagaIndice($info, $id)
+{
+    $this->db->where('Id_Usuario', $IdUsuario);
+    $this->db->update('TabUsuario', $infoUsuario);
+    
+    return $this->db->affected_rows();
+}
+// FIM DAS CONSULTAS NA TELA DE INDICE
+
+}
+
+  
---
+++ application/views/qualicad/principal/l_principalConvenio.php
@@ -0,0 +1,116 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      <i class="fa fa-users"></i> Listar Convênios
+      <small>Listar</small>
+    </h1>
+  </section>
+  <section class="content">
+    <div class="col-xs-12">
+      <div class="text-right">
+        <a class="btn btn-primary" href="<?php echo base_url(); ?>principalConvenio/cadastrar">
+          <i class="fa fa-plus"></i> Adicionar convênio</a>
+      </div>
+      <div class="box">
+        <div class="box-header">
+          <div class="box-tools">
+          </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body table-responsive no-padding">
+          <?php
+                    $this->load->helper('form');
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                ?>
+            <div class="alert alert-danger alert-dismissable">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+              <?php echo $this->session->flashdata('error'); ?>
+            </div>
+            <?php } ?>
+            <?php  
+                    $success = $this->session->flashdata('success');
+                    if($success)
+                    {
+                ?>
+            <div class="alert alert-success alert-dismissable">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+              <?php echo $this->session->flashdata('success'); ?>
+            </div>
+            <?php } ?>
+            <div class="panel-body">
+              <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>CNPJ</th>
+                    <th>Código ERP</th>
+                    <th>Tipo</th>
+                    <th>Convênio ativo?</th>
+                    <th>Data de início</th>
+                    <th>Data de vigência</th>
+                    <th>Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                      if(!empty($registrosUsuarios))
+                      {
+                          foreach($registrosUsuarios as $registro)
+                          {
+                      ?>
+                    <tr>
+                      <td>
+                        <?php echo $registro->Id_Usuario ?>
+                      </td>
+                      <td>
+                        <?php echo $registro->Nome_Usuario ?>
+                      </td>
+                      <td>
+                        <?php echo $registro->Cpf_Usuario ?>
+                      </td>
+                      <td>
+                        <?php echo $registro->Email ?>
+                      </td>
+                      <td>
+                        <?php echo ($registro->Tp_Ativo == 'S') ? 'Sim' : 'Não'; ?>
+                      </td>
+                      <td>
+                        <?php echo ($registro->Admin == 'S') ? 'Sim' : 'Não'; ?>
+                      </td>
+                      <td>
+                        <?php echo ($registro->Dt_Ativo != null) ? date("d/m/Y", strtotime($registro->Dt_Ativo)) : ''; ?>
+                      </td>
+                      <td>
+                        <?php echo ($registro->Dt_Inativo != null) ? date("d/m/Y", strtotime($registro->Dt_Inativo)) : ''; ?>
+                      </td>
+                      <td class="text-center">
+                        <!--  <a class="btn btn-sm btn-primary" href="<?php //echo base_url().'log-history/'.$record->userId; ?>" title="Log geçmişi">
+                              <i class="fa fa-history"></i>
+                          </a> -->
+                          <a class="btn btn-sm btn-info" href="<?php echo base_url().'cadastroUsuario/editar/'.$registro->Id_Usuario; ?>" title="Editar">
+                              <i class="fa fa-pencil"></i>
+                          </a>
+                          <a class="btn btn-sm btn-danger deleteUser" href="<?php echo base_url().'apagaUsuario/'.$registro->Id_Usuario; ?>" data-userid="<?php echo $registro->Id_Usuario; ?>" title="Excluir">
+                              <i class="fa fa-trash-o"></i>
+                          </a>
+                      </td>
+                    </tr>
+                    <?php
+                          }
+                      }
+                      ?>
+                </tbody>
+              </table>
+            </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+</div>
+</section>
+</div>
