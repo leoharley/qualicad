86a04df928256a5c0c3a4d3ee80088c2c92f101c
Leonardo <leoharleygoncalves@gmail.com>
2022-03-02 22:51:52 -0300

    ...

--- application/models/PrincipalModel.php
+++ application/models/PrincipalModel.php
@@ -56,24 +56,17 @@ class PrincipalModel extends CI_Model
 // INICIO DAS CONSULTAS NA TELA DE PLANO
 function listaPlano($id, $searchText = '', $page, $segment)
 {
-    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
-    $this->db->from('TabUsuario as Usuarios');
+    $this->db->select('*');
+    $this->db->from('TbPlano as Plano');
 //     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
     if(!empty($searchText)) {
-        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
-                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
-                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $likeCriteria = "(Plano.Ds_Plano LIKE '%".$searchText."%')";
         $this->db->where($likeCriteria);
     }
-    $campos = "((Usuarios.Admin = 'S'
-                AND Usuarios.CriadoPor = '".$idUser."'))
-                OR
-                (Usuarios.Admin = 'N')";
-    $this->db->where($campos);
 
-    $this->db->where('Usuarios.Deletado !=', 'S');
-    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
-    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->where('Plano.Deletado !=', 'S');
+    $this->db->where('Plano.Tp_Ativo', 'S');
+    $this->db->where('Plano.CriadoPor', $id);
     $this->db->limit($page, $segment);
     $query = $this->db->get();
     
@@ -113,24 +106,17 @@ function apagaPlano($info, $id)
 // INICIO DAS CONSULTAS NA TELA DE FATURAMENTO
 function listaFaturamento($id, $searchText = '', $page, $segment)
 {
-    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
-    $this->db->from('TabUsuario as Usuarios');
+    $this->db->select('*');
+    $this->db->from('TbFaturamento as Faturamento');
 //     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
     if(!empty($searchText)) {
-        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
-                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
-                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $likeCriteria = "(Faturamento.Ds_Faturamento  LIKE '%".$searchText."%')";
         $this->db->where($likeCriteria);
     }
-    $campos = "((Usuarios.Admin = 'S'
-                AND Usuarios.CriadoPor = '".$idUser."'))
-                OR
-                (Usuarios.Admin = 'N')";
-    $this->db->where($campos);
-
-    $this->db->where('Usuarios.Deletado !=', 'S');
-    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
-    $this->db->where('Usuarios.CriadoPor', $idUser);
+    
+    $this->db->where('Faturamento.Deletado !=', 'S');
+    $this->db->where('Faturamento.Tp_Ativo', 'S');
+    $this->db->where('Faturamento.CriadoPor', $id);
     $this->db->limit($page, $segment);
     $query = $this->db->get();
     
@@ -170,24 +156,17 @@ function apagaFaturamento($info, $id)
 // INICIO DAS CONSULTAS NA TELA DE REGRA
 function listaRegra($id, $searchText = '', $page, $segment)
 {
-    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
-    $this->db->from('TabUsuario as Usuarios');
+    $this->db->select('*');
+    $this->db->from('TbRegra as Regra');
 //     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
     if(!empty($searchText)) {
-        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
-                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
-                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $likeCriteria = "(Regra.Ds_Regra LIKE '%".$searchText."%')";
         $this->db->where($likeCriteria);
     }
-    $campos = "((Usuarios.Admin = 'S'
-                AND Usuarios.CriadoPor = '".$idUser."'))
-                OR
-                (Usuarios.Admin = 'N')";
-    $this->db->where($campos);
 
-    $this->db->where('Usuarios.Deletado !=', 'S');
-    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
-    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->where('Regra.Deletado !=', 'S');
+    $this->db->where('Regra.Tp_Ativo', 'S');
+    $this->db->where('Regra.CriadoPor', $id);
     $this->db->limit($page, $segment);
     $query = $this->db->get();
     
@@ -227,24 +206,17 @@ function apagaRegra($info, $id)
 // INICIO DAS CONSULTAS NA TELA DE INDICE
 function listaIndice($id, $searchText = '', $page, $segment)
 {
-    $this->db->select('Usuarios.Id_Usuario, Usuarios.Nome_Usuario, Usuarios.Admin, Usuarios.Cpf_Usuario, Usuarios.Tp_Ativo, Usuarios.Dt_Ativo, Usuarios.Dt_Inativo, Usuarios.Email');
-    $this->db->from('TabUsuario as Usuarios');
+    $this->db->select('*');
+    $this->db->from('TbIndice as Indice');
 //     $this->db->join('tbl_roles as Role', 'Role.roleId = Usuarios.roleId','left');
     if(!empty($searchText)) {
-        $likeCriteria = "(Usuarios.Email  LIKE '%".$searchText."%'
-                        OR  Usuarios.Nome_Usuario  LIKE '%".$searchText."%'
-                        OR  Usuarios.Cpf_Usuario  LIKE '%".$searchText."%')";
+        $likeCriteria = "(Indice.Ds_Indice LIKE '%".$searchText."%')";
         $this->db->where($likeCriteria);
     }
-    $campos = "((Usuarios.Admin = 'S'
-                AND Usuarios.CriadoPor = '".$idUser."'))
-                OR
-                (Usuarios.Admin = 'N')";
-    $this->db->where($campos);
 
-    $this->db->where('Usuarios.Deletado !=', 'S');
-    $this->db->where('Usuarios.Id_Usuario !=', $idUser);
-    $this->db->where('Usuarios.CriadoPor', $idUser);
+    $this->db->where('Indice.Deletado !=', 'S');
+    $this->db->where('Indice.Tp_Ativo', 'S');
+    $this->db->where('Indice.CriadoPor', $id);
     $this->db->limit($page, $segment);
     $query = $this->db->get();
     
