e24d84768904d89184effe01fb1a662ff9c24c2c
Leonardo <leoharleygoncalves@gmail.com>
2022-03-05 12:49:33 -0300

    ...

--- application/views/qualicad/principal/c_principalIndice.php
+++ application/views/qualicad/principal/c_principalIndice.php
@@ -163,8 +163,17 @@ if(!empty($infoIndice))
     </section>
 </div>
 <script src="<?php echo base_url(); ?>assets/js/addUser.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/shortcut.js"></script>
 <script>
 $(document).ready(function(){
     $(":input").inputmask();
 });
+
+    shortcut.add("alt+s", function() {
+        alert('teste');
+    });   
+    shortcut.add("ctrl+enter", function() {
+        // Do something
+    }); 
+
 </script>
