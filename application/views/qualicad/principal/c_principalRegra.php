115d76974512da8395a2ae98cd01ae5ad28710dd
Leonardo <leoharleygoncalves@gmail.com>
2022-03-04 23:53:38 -0300

    ...

--- application/views/qualicad/principal/c_principalRegra.php
+++ application/views/qualicad/principal/c_principalRegra.php
@@ -34,7 +34,27 @@ if(!empty($infoRegra))
             <div class="col-md-8">
                 <!-- general form elements -->
 
-
+                <?php
+                    $this->load->helper('form');
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                ?>
+                <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <?php echo $this->session->flashdata('error'); ?>
+                </div>
+                <?php } ?>
+                <?php  
+                        $success = $this->session->flashdata('success');
+                        if($success)
+                        {
+                    ?>
+                <div class="alert alert-success alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <?php echo $this->session->flashdata('success'); ?>
+                </div>
+                <?php } ?>
 
                 <div class="box box-primary">
                     <div class="box-header">
