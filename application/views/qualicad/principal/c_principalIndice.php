823ff6027ba9a4d96ae288e5bb6fcce593ead857
Leonardo <leoharleygoncalves@gmail.com>
2022-03-04 22:56:53 -0300

    ...

--- application/views/qualicad/principal/c_principalIndice.php
+++ application/views/qualicad/principal/c_principalIndice.php
@@ -42,7 +42,7 @@ span {
 
 span:after {
     font-family: FontAwesome;
-    content: "\f0a9"; /* Value may need to be changed in newer version of font awesome*/
+    content: "\f356"; /* Value may need to be changed in newer version of font awesome*/
     font-size: 13px;
     position: absolute;
     right: 20px;
