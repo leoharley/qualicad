d29b8fa00faee9643bd6458a0bfbf4e86c8ea908
Leonardo <leoharleygoncalves@gmail.com>
2022-03-05 13:28:41 -0300

    ...

--- application/views/qualicad/principal/c_principalPlano.php
+++ application/views/qualicad/principal/c_principalPlano.php
@@ -180,10 +180,10 @@ if(!empty($infoPlano))
                         <!-- /.box-body -->
 
                         <div class="box-footer">
-                            <input type="submit" class="btn btn-primary" value="Ir para lista (CTRL+L)" name="IrLista" id="IrLista"/>
-                            <input type="submit" class="btn btn-primary" value="Salvar e ir para lista (CTRL+S)" name="salvarIrLista" id="salvarIrLista" style="margin-left:5px;"/>
-                            <input type="submit" class="btn btn-primary" value="Salvar e cadastrar outro plano (CTRL+A)" name="salvarMesmaTela" id="salvarMesmaTela" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'editar') { echo 'display:none'; } ?>"/>
-                            <input type="submit" class="btn btn-primary" value="Salvar e cadastrar convênio (CTRL+C)" name="salvarRetroceder" id="salvarRetroceder" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'editar') { echo 'display:none'; } ?>"/>
+                            <input type="submit" class="btn btn-primary" value="Lista (CTRL+L)" name="IrLista" id="IrLista"/>
+                            <input type="submit" class="btn btn-primary" value="Salva e lista (CTRL+S)" name="salvarIrLista" id="salvarIrLista" style="margin-left:5px;"/>
+                            <input type="submit" class="btn btn-primary" value="Salva e cadastra novamente (CTRL+A)" name="salvarMesmaTela" id="salvarMesmaTela" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'editar') { echo 'display:none'; } ?>"/>
+                            <input type="submit" class="btn btn-primary" value="Salva e cadastra convênio (CTRL+C)" name="salvarRetroceder" id="salvarRetroceder" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'editar') { echo 'display:none'; } ?>"/>
                         <!--    <input type="reset" class="btn btn-info" value="Limpar Campos" /> -->
                         </div>
                     </form>
