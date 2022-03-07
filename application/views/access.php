346cc8e1c31f6f626f01c4fc70dde5e82aa5dc5c
Leonardo <leoharleygoncalves@gmail.com>
2022-03-07 20:56:31 -0300

    ...

--- application/views/access.php
+++ application/views/access.php
@@ -2,14 +2,13 @@
     <section class="content-header">
         <h1>
             Acesso negado
-            <small>Você não está autorizado à acessar essa página.</small>
         </h1>
     </section>
     <section class="content">
         <div class="row">
             <div class="col-xs-12 text-center">
                 <br/><br/><br/>
-                <h2 style="color:red"><b>O ACESSO A ESTÁ PÁGINA NÃO É PERMITIDO</b></h2>
+                <h2 style="color:red">O ACESSO A ESTÁ PÁGINA NÃO É PERMITIDO!</h2>
             </div>
         </div>
     </section>
