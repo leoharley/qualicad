6c4339f08ab178734b3ad5050e21ee41c8cfacc3
Leonardo <leoharleygoncalves@gmail.com>
2022-03-07 21:19:19 -0300

    ...

--- application/views/access.php
+++ application/views/access.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-xs-12 text-center">
                 <br/><br/>
-                <img src="https://cdn1.iconfinder.com/data/icons/jetflat-multimedia-vol-4/90/0042_099_lock_access_denied_blocked-512.png" width="20%"/>
+                <img src="https://media.istockphoto.com/vectors/shield-with-hand-block-icon-in-flat-style-with-shadow-vector-id867726980?k=20&m=867726980&s=612x612&w=0&h=ge5fg-rTBLy_OVAyhCiawo4Ou9bHcIQ9WtVORH20N8U=" width="20%"/>
                 <br/>
                 <h2 style="color:red">ESTA AÇÃO NÃO É PERMITIDA!</h2>
             </div>
