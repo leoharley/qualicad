f3aea72aaecee5f9ae1b7ae4235e8abfd6be8ac0
Leonardo <leoharleygoncalves@gmail.com>
2022-03-07 22:45:55 -0300

    ...

--- application/views/telaNaoAutorizada.php
+++ application/views/telaNaoAutorizada.php
@@ -10,7 +10,7 @@
                 <br/><br/>
                 <img src="https://cdn1.iconfinder.com/data/icons/jetflat-multimedia-vol-4/90/0042_099_lock_access_denied_blocked-512.png" width="20%"/>
                 <br/>
-                <h2 style="color:red">ESTA AÇÃO NÃO É AUTORIZADA!</h2>
+                <h2 style="color:red">VISUALIZAÇÃO DE TELA NÃO AUTORIZADA!</h2>
             </div>
         </div>
     </section>
