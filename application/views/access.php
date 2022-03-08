1787148c71b82f12bf5d61c110cccafe04ab2e6d
Leonardo <leoharleygoncalves@gmail.com>
2022-03-07 21:01:06 -0300

    ...

--- application/views/access.php
+++ application/views/access.php
@@ -10,7 +10,7 @@
                 <br/><br/>
                 <img src="https://cdn1.iconfinder.com/data/icons/jetflat-multimedia-vol-4/90/0042_099_lock_access_denied_blocked-512.png" width="20%"/>
                 <br/>
-                <h2 style="color:red">O ACESSO A ESTA PÁGINA NÃO ESTÁ AUTORIZADO!</h2>
+                <h2 style="color:red">O ACESSO A ESTA AÇÃO NÃO ESTÁ AUTORIZADO!</h2>
             </div>
         </div>
     </section>
