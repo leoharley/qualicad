f526b9ec6d9d99b407bf99972d3a0d04538551ac
Leonardo <leoharleygoncalves@gmail.com>
2022-03-11 00:22:21 -0300

    ...

--- application/views/qualicad/principal/l_principalFracaoSimproBra.php
+++ application/views/qualicad/principal/l_principalFracaoSimproBra.php
@@ -98,7 +98,7 @@
                         <?php echo $registro->Qt_Prod ?>
                       </td>
                       <td>
-                        <?php echo ($registro->Tp_Ativo == 'S') ? 'Sim' : 'Não'; ?>
+                        <?php echo $registro->Tp_Ativo ?>
                       </td>
                       <td class="text-center">
                         <!--  <a class="btn btn-sm btn-primary" href="<?php //echo base_url().'log-history/'.$record->userId; ?>" title="Log geçmişi">
