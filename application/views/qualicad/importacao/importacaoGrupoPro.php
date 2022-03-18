faa65b9133df618a2458e1cd257c6648c85b8c98
Leonardo <leoharleygoncalves@gmail.com>
2022-03-17 21:12:53 -0300

    ...

--- application/views/qualicad/importacao/importacaoGrupoPro.php
+++ application/views/qualicad/importacao/importacaoGrupoPro.php
@@ -1,45 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Laravel Import Excel CSV to MySQL</title>
-	<meta name="description" content="The tiny framework with powerful features">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>CodeIgniter CSV Import</title>
 	
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<style>
-	  .container {
-		max-width: 500px;
-	  }
-	</style>
+    <!-- Bootstrap library -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/bootstrap.min.css'); ?>">
+    
+    <!-- Stylesheet file -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 </head>
 <body>
-<div class="container mt-5">
-	<div class="card">
-		<div class="card-header text-center">
-			<strong>Upload CSV File</strong>
-		</div>
-		<div class="card-body">
-		<div class="mt-2">
-			<?php if (session()->has('message')){ ?>
-				<div class="alert <?=session()->getFlashdata('alert-class') ?>">
-					<?=session()->getFlashdata('message') ?>
-				</div>
-			<?php } ?>
-			<?php $validation = \Config\Services::validation(); ?>
-		</div>	
-			<form action="<?=site_url('import-csv') ?>" method="post" enctype="multipart/form-data">
-				<div class="form-group mb-3">
-					<div class="mb-3">
-						<input type="file" name="file" class="form-control" id="file">
-					</div>					   
-				</div>
-				<div class="d-grid">
-					<input type="submit" name="submit" value="Upload" class="btn btn-dark" />
-				</div>
-			</form>
-		</div>
-	</div>
+<div class="container">
+    <h2>Members List</h2>
+	
+    <!-- Display status message -->
+    <?php if(!empty($success_msg)){ ?>
+    <div class="col-xs-12">
+        <div class="alert alert-success"><?php echo $success_msg; ?></div>
+    </div>
+    <?php if(!empty($error_msg)){ ?>
+    <div class="col-xs-12">
+        <div class="alert alert-danger"><?php echo $error_msg; ?></div>
+    </div>
+    <?php } ?>
+	
+    <div class="row">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+            <div class="float-right">
+                <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm');"><i class="plus"></i> Import</a>
+            </div>
+        </div>
+		
+        <!-- File upload form -->
+        <div class="col-md-12" id="importFrm" style="display: none;">
+            <form action="<?php echo base_url('members/import'); ?>" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" />
+                <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
+            </form>
+        </div>
+        
+        <!-- Data list table -->
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>#ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if(!empty($members)){ foreach($members as $row){ ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['phone']; ?></td>
+                    <td><?php echo $row['status']; ?></td>
+                </tr>
+                <?php } }else{ ?>
+                <tr><td colspan="5">No member(s) found...</td></tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
+
+<script>
+function formToggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
+</script>
 </body>
 </html>
