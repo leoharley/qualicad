d8cb228abb85ec1be1c0c5d92b434275400b7f46
Leonardo <leoharleygoncalves@gmail.com>
2022-03-17 23:36:12 -0300

    ...

--- application/views/qualicad/importacao/importacaoGrupoPro.php
+++ application/views/qualicad/importacao/importacaoGrupoPro.php
@@ -41,6 +41,7 @@
         <div class="col-md-12" id="importFrm">
             <form action="<?php echo base_url() ?>importaGrupoPro" method="post" enctype="multipart/form-data">
                 <input type="file" name="file" />
+                <br/>
                 <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORTAR">
             </form>
         </div>
