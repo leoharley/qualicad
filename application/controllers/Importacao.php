47c0b1bf7ef7e92f18bd3fb9eace432515e29db2
Leonardo <leoharleygoncalves@gmail.com>
2022-03-31 20:15:00 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -86,7 +86,7 @@ class Importacao extends BaseController
                                 $destino = isset($this->ImportacaoModel->consultaDePara('GrupoPro',$origem,$this->session->userdata('IdEmpresa'))[0]->No_CampoDestino);
                                 if (isset($destino)) {
                                     $memData += array(
-                                        $destino => $row[$origem]
+                                        strval($destino) => $row[strval($origem)]
                                     );
                                 }     
                             }
