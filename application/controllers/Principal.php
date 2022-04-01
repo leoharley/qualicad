e778c80c7ea6b304d5214ba84c9f2fc6431cbad4
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-04-01 10:33:15 -0300

    ...

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -193,14 +193,16 @@ class Principal extends BaseController
 
                 //'Senha'=>getHashedPassword($senha)
 
-                var_dump($Ds_Plano);exit;
+                if ($Ds_Plano != '') {
+                    $infoPlano = array('TbConvenio_Id_Convenio' => $TbConvenio_Id_Convenio, 'TbEmpresa_Id_Empresa' => $this->session->userdata('IdEmpresa'),
+                        'Ds_Plano' => $Ds_Plano, 'TbIndice_Id_Indice' => $TbIndice_Id_Indice, 'TbRegra_Id_Regra' => $TbRegra_Id_Regra, 'Cd_PlanoERP' => $Cd_PlanoERP,
+                        'Tp_AcomodacaoPadrao' => $Tp_AcomodacaoPadrao, 'CriadoPor' => $this->vendorId, 'AtualizadoPor' => $this->vendorId,
+                        'Tp_Ativo' => $Tp_Ativo, 'Dt_Ativo' => $Dt_Ativo);
 
-                $infoPlano = array('TbConvenio_Id_Convenio'=>$TbConvenio_Id_Convenio,  'TbEmpresa_Id_Empresa'=>$this->session->userdata('IdEmpresa'),
-                    'Ds_Plano'=>$Ds_Plano, 'TbIndice_Id_Indice'=> $TbIndice_Id_Indice, 'TbRegra_Id_Regra'=> $TbRegra_Id_Regra, 'Cd_PlanoERP'=>$Cd_PlanoERP,
-                    'Tp_AcomodacaoPadrao'=>$Tp_AcomodacaoPadrao, 'CriadoPor'=>$this->vendorId, 'AtualizadoPor'=>$this->vendorId,
-                    'Tp_Ativo'=>$Tp_Ativo, 'Dt_Ativo'=>$Dt_Ativo);
-
-                $resultPlano = $this->PrincipalModel->adicionaPlano($infoPlano);
+                    $resultPlano = $this->PrincipalModel->adicionaPlano($infoPlano);
+                } else {
+                    $resultPlano = 1;
+                }
 
                 /*FIM ADICIONAR PLANO*/
                 
