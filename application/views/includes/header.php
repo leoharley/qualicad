e31129203cca892e2b3bb08b2f8c225efd1f5334
Leonardo <leoharleygoncalves@gmail.com>
2022-04-05 23:49:19 -0300

    ...

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -415,7 +415,7 @@ $(function() {
                     <li class="treeview">
                         <a href="'.base_url().'importacaoDePara/listar">
                             <i class="fa fa-th"></i>
-                            <span>Configurar DePara</span>
+                            <span style="color:red"><strong>Configurar DePara</strong></span>
                         </a>
                     </li>';
                   }
