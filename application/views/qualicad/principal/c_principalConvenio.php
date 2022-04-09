347c38f14f1f74b118f388ad481bbdb0b0ab9b4d
Leonardo <leoharleygoncalves@gmail.com>
2022-04-09 15:38:28 -0300

    ...

--- application/views/qualicad/principal/c_principalConvenio.php
+++ application/views/qualicad/principal/c_principalConvenio.php
@@ -168,7 +168,7 @@ if(!empty($infoConvenio))
                             background-color: #f5f5f5;">
 
                                 <h4><strong>Plano</strong>
-                                <input type="submit" class="btn btn-sm btn-primary" value="Adicionar" name="salvarPlano" id="salvarPlano" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'cadastrar'){echo 'display:none';}?>"/>
+                            <!--    <input type="submit" class="btn btn-sm btn-primary" value="Adicionar" name="salvarPlano" id="salvarPlano" style="margin-left:5px;<?php //if ($this->uri->segment(2) == 'cadastrar'){echo 'display:none';}?>"/> -->
                                 </h4>
 
                                 <table style="width:100%;">
