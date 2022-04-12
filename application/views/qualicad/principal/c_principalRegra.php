5554eb8f71ce6d2653b99d5b8288b8b50cfc1058
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-04-12 15:47:23 -0300

    ...

--- application/views/qualicad/principal/c_principalRegra.php
+++ application/views/qualicad/principal/c_principalRegra.php
@@ -244,6 +244,7 @@ if(!empty($infoRegra))
                             <input type="button" class="btn btn-primary" onclick="window.location='<?php echo base_url(); ?>principalRegra/listar';" value="Lista (CTRL+L)" name="IrLista" id="IrLista"/>
                             <input type="submit" class="btn btn-primary" value="Salva e lista (CTRL+S)" name="salvarIrLista" id="salvarIrLista" style="margin-left:5px;"/>
                             <input type="submit" class="btn btn-primary" value="Salva e cadastra novamente (CTRL+A)" name="salvarMesmaTela" id="salvarMesmaTela" style="margin-left:5px;<?php if ($this->uri->segment(2) == 'editar') { echo 'display:none'; } ?>"/>
+                            <input type="submit" class="btn btn-primary" value="Salva e adiciona outro regra grupro (CTRL+P)" name="salvarRegraGruPro" id="salvarRegraGruPro" style="margin-left:5px;"/>
                     <!--    <input type="reset" class="btn btn-info" value="Limpar Campos" /> -->
                         </div>
                     </form>
@@ -270,5 +271,8 @@ if(!empty($infoRegra))
     });
     shortcut.add("ctrl+a", function() {
         document.getElementById('salvarMesmaTela').click();
+    });
+    shortcut.add("ctrl+p", function() {
+        document.getElementById('salvarRegraGruPro').click();
     });
 </script>
