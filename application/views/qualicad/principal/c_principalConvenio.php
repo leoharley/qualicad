0d0223c9b3674149f382dd1de3b398ca33b8016f
Leonardo <leoharleygoncalves@gmail.com>
2022-04-18 22:22:06 -0300

    ....

--- application/views/qualicad/principal/c_principalConvenio.php
+++ application/views/qualicad/principal/c_principalConvenio.php
@@ -112,7 +112,7 @@ if(!empty($infoConvenio))
                                 <div class="col-md-1">
                                     <div class="form-group">
                                         <label for="Id_Convenio">Código</label>
-                                        <input type="text" class="form-control required" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Id_Convenio') : $Id_Convenio ; ?>" id="Id_Convenio" name="Id_Convenio">
+                                        <input type="text" class="form-control required" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Id_Convenio') : $Id_Convenio ; ?>" id="Id_Convenio" name="Id_Convenio" disabled>
                                     </div>
                                 </div>
                                 <div class="col-md-2">
