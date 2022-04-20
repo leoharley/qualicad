8f2942142d6bed26d8df632cde4d3d56ff8128e3
Leonardo <leoharleygoncalves@gmail.com>
2022-04-19 22:20:47 -0300

    ...

--- application/views/qualicad/principal/c_principalPlano.php
+++ application/views/qualicad/principal/c_principalPlano.php
@@ -75,108 +75,128 @@ if(!empty($infoPlano))
                     <?php $this->load->helper("form"); ?>
                     <form role="form" id="addPlano" action="<?php echo ($this->uri->segment(2) == 'cadastrar') ? base_url().'adicionaPlano' : base_url().'editaPlano'; ?>" method="post" role="form">
                         <div class="box-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="TbConvenio_Id_Convenio">Convênio</label>
-                                        <select class="form-control required" id="TbConvenio_Id_Convenio" name="TbConvenio_Id_Convenio">
-                                            <?php
-                                            if(!empty($infoConvenio))
-                                            {
-                                                foreach ($infoConvenio as $convenio)
-                                                {
-                                                    ?>
-                                                <option value="<?php echo $convenio->Id_Convenio ?>" <?php if ($this->uri->segment(2) == 'editar' && $convenio->Id_Convenio == $TbConvenio_Id_Convenio) { echo 'selected'; } ?>>
-                                                    <?php echo $convenio->Id_Convenio.' - '.$convenio->Ds_Convenio ?>
-                                                </option>
+
+                        <div class="row" style="display: inline-block;width: 98%;height: 100%;margin: 0.15rem;padding-top: 0.85rem;padding-left:1rem;padding-right:1rem;
+                            background-color: #f5f5f5;padding-bottom:2rem">
+
+                                <h4><strong>Plano</strong></h4>
+
+                                <table style="width:100%;">
+                                    <thead>
+                                    <tr style="background-color:#e0e0e0">
+                                        <th class="header-label" style="padding:10px">
+                                        Código
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Convênio
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Índice
+                                        </th>                                        
+                                        <th class="header-label" style="padding:10px">
+                                        Regra
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Descrição do plano
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Código ERP
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Tipo de acomodação padrão
+                                        </th>
+                                        <th class="header-label" style="padding:10px">
+                                        Plano ativo?
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tr id="row0">
+                                            <td>
+                                                <select class="form-control required" id="TbConvenio_Id_Convenio" name="TbConvenio_Id_Convenio">
                                                 <?php
-                                                }
-                                            }
-                                            ?>
-                                        </select>
-                                        <input type="hidden" value="<?php echo $Id_Plano; ?>" name="Id_Plano" id="Id_Plano" />
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="TbIndice_Id_Indice">Índice</label>
-                                        <select class="form-control required" id="TbIndice_Id_Indice" name="TbIndice_Id_Indice">
-                                            <?php
-                                            if(!empty($infoIndice))
-                                            {
-                                                foreach ($infoIndice as $indice)
+                                                if(!empty($infoConvenio))
                                                 {
-                                                    ?>
-                                                <option value="<?php echo $indice->Id_Indice ?>" <?php if ($this->uri->segment(2) == 'editar' && $indice->Id_Indice == $TbIndice_Id_Indice) { echo 'selected'; } ?>>
-                                                    <?php echo $indice->Id_Indice.' - '.$indice->Ds_indice ?>
-                                                </option>
-                                                <?php
+                                                    foreach ($infoConvenio as $convenio)
+                                                    {
+                                                        ?>
+                                                    <option value="<?php echo $convenio->Id_Convenio ?>" <?php if ($this->uri->segment(2) == 'editar' && $convenio->Id_Convenio == $TbConvenio_Id_Convenio) { echo 'selected'; } ?>>
+                                                        <?php echo $convenio->Id_Convenio.' - '.$convenio->Ds_Convenio ?>
+                                                    </option>
+                                                    <?php
+                                                    }
                                                 }
-                                            }
-                                            ?>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="TbRegra_Id_Regra">Regra</label>
-                                        <select class="form-control required" id="TbRegra_Id_Regra" name="TbRegra_Id_Regra">
-                                            <?php
-                                            if(!empty($infoRegra))
-                                            {
-                                                foreach ($infoRegra as $regra)
+                                                ?>
+                                                </select>
+                                                <input type="hidden" value="<?php echo $Id_Plano; ?>" name="Id_Plano" id="Id_Plano" />
+                                            </td>
+
+                                            <td>
+                                                <select class="form-control required" id="TbIndice_Id_Indice" name="TbIndice_Id_Indice">
+                                                <?php
+                                                if(!empty($infoIndice))
                                                 {
-                                                    ?>
-                                                <option value="<?php echo $regra->Id_Regra ?>" <?php if ($this->uri->segment(2) == 'editar' && $regra->Id_Regra == $TbRegra_Id_Regra) { echo 'selected'; } ?>>
-                                                    <?php echo $regra->Id_Regra.' - '.$regra->Ds_Regra ?>
-                                                </option>
-                                                <?php
+                                                    foreach ($infoIndice as $indice)
+                                                    {
+                                                        ?>
+                                                    <option value="<?php echo $indice->Id_Indice ?>" <?php if ($this->uri->segment(2) == 'editar' && $indice->Id_Indice == $TbIndice_Id_Indice) { echo 'selected'; } ?>>
+                                                        <?php echo $indice->Id_Indice.' - '.$indice->Ds_indice ?>
+                                                    </option>
+                                                    <?php
+                                                    }
                                                 }
-                                            }
-                                            ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="Ds_Plano">Descrição do plano</label>
-                                        <input type="text" class="form-control required" id="Ds_Plano" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Ds_Plano') : $Ds_Plano ; ?>" name="Ds_Plano"
+                                                ?>
+                                                </select>
+                                            </td>
+
+                                            <td>
+                                                <select class="form-control required" id="TbRegra_Id_Regra" name="TbRegra_Id_Regra">
+                                                <?php
+                                                if(!empty($infoRegra))
+                                                {
+                                                    foreach ($infoRegra as $regra)
+                                                    {
+                                                        ?>
+                                                    <option value="<?php echo $regra->Id_Regra ?>" <?php if ($this->uri->segment(2) == 'editar' && $regra->Id_Regra == $TbRegra_Id_Regra) { echo 'selected'; } ?>>
+                                                        <?php echo $regra->Id_Regra.' - '.$regra->Ds_Regra ?>
+                                                    </option>
+                                                    <?php
+                                                    }
+                                                }
+                                                ?>
+                                                </select>
+                                            </td>
+
+                                            <td>
+                                            <input type="text" class="form-control required" id="Ds_Plano" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Ds_Plano') : $Ds_Plano ; ?>" name="Ds_Plano"
                                             maxlength="128">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="Cd_PlanoERP">Código ERP</label>
-                                        <input type="text" class="form-control required" id="Cd_PlanoERP" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Cd_PlanoERP') : $Cd_PlanoERP ; ?>" name="Cd_PlanoERP"
+                                            </td>
+
+                                            <td>
+                                            <input type="text" class="form-control required" id="Cd_PlanoERP" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('Cd_PlanoERP') : $Cd_PlanoERP ; ?>" name="Cd_PlanoERP"
                                             maxlength="11">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="Tp_AcomodacaoPadrao">Tipo de acomodação padrão</label>
-                                        <select class="form-control required" id="Tp_AcomodacaoPadrao" name="Tp_AcomodacaoPadrao">
-                                            <option value="1" <?php if ($this->uri->segment(2) == 'editar' && $Tp_AcomodacaoPadrao == '1') { echo 'selected'; } else if ($this->uri->segment(2) == 'cadastrar') { echo 'selected'; } ?>>Enfermaria</option>
-                                            <option value="2" <?php if ($this->uri->segment(2) == 'editar' && $Tp_AcomodacaoPadrao == '2') { echo 'selected'; } ?>>Apartamento</option>
-                                        </select>
-                                    </div>
-                                </div>
+                                            </td>
+
+                                            <td>
+                                                <select class="form-control required" id="Tp_AcomodacaoPadrao" name="Tp_AcomodacaoPadrao">
+                                                <option value="1" <?php if ($this->uri->segment(2) == 'editar' && $Tp_AcomodacaoPadrao == '1') { echo 'selected'; } else if ($this->uri->segment(2) == 'cadastrar') { echo 'selected'; } ?>>Enfermaria</option>
+                                                <option value="2" <?php if ($this->uri->segment(2) == 'editar' && $Tp_AcomodacaoPadrao == '2') { echo 'selected'; } ?>>Apartamento</option>
+                                                </select>
+                                            </td>
+
+                                            <td>
+                                                <select class="form-control required" id="Tp_Ativo" name="Tp_Ativo">
+                                                <option value="S" <?php if ($this->uri->segment(2) == 'editar' && $Tp_Ativo == 'S') { echo 'selected'; } else if ($this->uri->segment(2) == 'cadastrar') { echo 'selected'; } ?>>Sim</option>
+                                                <option value="N" <?php if ($this->uri->segment(2) == 'editar' && $Tp_Ativo == 'N') { echo 'selected'; } ?>>Não</option>
+                                                </select>
+                                            </td>
+
+                                </tr>
+
+                                </table>
                             </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="Tp_Ativo">Plano ativo?</label>
-                                        <select class="form-control required" id="Tp_Ativo" name="Tp_Ativo">
-                                            <option value="S" <?php if ($this->uri->segment(2) == 'editar' && $Tp_Ativo == 'S') { echo 'selected'; } else if ($this->uri->segment(2) == 'cadastrar') { echo 'selected'; } ?>>Sim</option>
-											<option value="N" <?php if ($this->uri->segment(2) == 'editar' && $Tp_Ativo == 'N') { echo 'selected'; } ?>>Não</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            
+                        
+                        
                         <!-- /.box-body -->
 
                         <div class="box-footer">
