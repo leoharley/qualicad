2519430b847e1dcf672b1a0788c7dd74209d1458
Leonardo <leoharleygoncalves@gmail.com>
2022-04-22 17:06:39 -0300

    ...

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -3445,6 +3445,8 @@ class Principal extends BaseController
 
         $carregaGrupoPro = $this->PrincipalModel->carregaInfoGrupoProTpGrupoPro($Tp_GrupoPro,$this->session->userdata('IdEmpresa'));
 
+
+        var_dump($carregaGrupoPro);exit;
         if ($carregaGrupoPro != null) {
         foreach ($carregaGrupoPro as $data){
 
