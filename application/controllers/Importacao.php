af9a5b2d715625afc81baeb55e5243e55988d0df
Leonardo <leoharleygoncalves@gmail.com>
2022-04-23 14:49:19 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -1058,6 +1058,8 @@ class Importacao extends BaseController
                     $csvData = $this->csvreader->parse_csv($_FILES['file']['tmp_name']);
                     $dePara = $this->ImportacaoModel->consultaDePara($this->input->post('Tb_Id_LayoutImportacao'),'FatItem',$this->session->userdata('IdEmpresa'));
 
+                    var_dump ($csvData);exit;
+
                     // Insert/update CSV data into database
                     if(!empty($csvData)){
                         foreach($csvData as $row) {
@@ -1079,8 +1081,6 @@ class Importacao extends BaseController
 
 
                             $insert = $this->ImportacaoModel->adicionaFatItem($memData);
-
-                            var_dump ($insert);exit;
 
                             if($insert){
                                 $insertCount++;
