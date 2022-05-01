f5981ee0ba663d4e79e55741d2b0b97fd320ffd3
Leonardo <leoharleygoncalves@gmail.com>
2022-05-01 18:31:32 -0300

    ...

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -1946,6 +1946,8 @@ class Principal extends BaseController
 
             $result = $this->PrincipalModel->adicionaIndiceGrupoPro($infoIndiceGrupoPro);
 
+            var_dump($result);exit;
+
             if($result > 0)
             {
                 $process = 'Adicionar índice grupo pro';
