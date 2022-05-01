627f9421e06a8291316974388b00cf35c08155c7
Leonardo <leoharleygoncalves@gmail.com>
2022-05-01 19:51:58 -0300

    ...

--- application/models/PrincipalModel.php
+++ application/models/PrincipalModel.php
@@ -494,9 +494,8 @@ function carregaInfoIndicesEmpresa($idEmpresa)
     {
         $this->db->select('*');
         $this->db->from('TbIndiceGrupo as IndiceGruPro');
-        $this->db->join('TbUsuEmp as UsuEmp', 'UsuEmp.Id_UsuEmp = IndiceGruPro.TbUsuEmp_Id_UsuEmp','inner');
-        $campos = "(IndiceGruPro.TbGrupoPro_CodGrupo = '".$TbGrupoPro_CodGrupo."'
-                    AND UsuEmp.TbEmpresa_Id_Empresa  = '".$IdEmpresa."')";
+        
+        $campos = "(IndiceGruPro.TbGrupoPro_CodGrupo = '".$TbGrupoPro_CodGrupo."')";
         $this->db->where($campos);
         $this->db->where('IndiceGruPro.Deletado !=', 'S');
         $query = $this->db->get();
