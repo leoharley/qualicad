50ece4165b3044684693a9e2b9831bae131b5073
Leonardo <leoharleygoncalves@gmail.com>
2022-05-05 20:15:35 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -80,7 +80,6 @@ class Importacao extends BaseController
                     $csvData = $this->csvreader->parse_csv($_FILES['file']['tmp_name']);
                     $dePara = $this->ImportacaoModel->consultaDePara(8,'Grupopro',$this->session->userdata('IdEmpresa'));
                     
-                    var_dump($csvData);exit;
                     // Insert/update CSV data into database
                     if(!empty($csvData)){
                         foreach($csvData as $row) {
@@ -100,6 +99,8 @@ class Importacao extends BaseController
                                     'TbEmpresa_Id_Empresa'=>$this->session->userdata('IdEmpresa'),
                                     'Tp_Ativo'=> 'S');
                                    
+
+                                    var_dump($memData);exit;
 
                                 $insert = $this->ImportacaoModel->adicionaGrupoPro($memData);
                             
