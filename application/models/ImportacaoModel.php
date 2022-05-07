69f3bf11068fb20b0b2551c75715863402e99d7a
Leonardo <leoharleygoncalves@gmail.com>
2022-05-07 15:10:48 -0300

    ...

--- application/models/ImportacaoModel.php
+++ application/models/ImportacaoModel.php
@@ -10,6 +10,7 @@ class ImportacaoModel extends CI_Model
         $this->db->where('GrupoPro.TbEmpresa_Id_Empresa', $idEmpresa);
         $this->db->where('GrupoPro.Deletado !=', 'S');
         $this->db->where('GrupoPro.Tp_Ativo', 'S');
+        $this->db->order_by('GrupoPro.Tp_GrupoPro', 'ASC');
         $query = $this->db->get();
 
         return $query->result();
