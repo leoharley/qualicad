b51c75e39678c59a8d973dca2f9af683b29ad7b4
Leonardo <leoharleygoncalves@gmail.com>
2022-05-07 21:35:32 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -338,6 +338,8 @@ class Importacao extends BaseController
 
                             $insert = $this->ImportacaoModel->adicionaTUSS($memData);
 
+                            var_dump ($memData);exit;
+
                             if($insert != 0){
                                 $insertCount++;
                             } else {
