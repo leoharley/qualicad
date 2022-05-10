1c0b305a6ef02e6761a98959bc037f2f15e21acc
Leonardo <leoharleygoncalves@gmail.com>
2022-05-10 20:39:46 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -56,11 +56,11 @@ class Exportacao extends BaseController
 
         $handle = fopen('php://output', 'w');
         fputcsv($handle, array('Id_FatItem', 'CodFatItem', 'TbFaturamento_Id_Faturamento'));
-        $i = 1;
+   /*     $i = 1;
         foreach ($exportacao as $data) {
             fputcsv($handle, array($i, $data["Id_FatItem"], $data["CodFatItem"], $data["TbFaturamento_Id_Faturamento"]));
             $i++;
-        }
+        }*/
             fclose($handle);
         exit;
     }
