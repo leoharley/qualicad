f308e4ceb55c3316bf6049fa29bcceea2761fed1
Leonardo <leoharleygoncalves@gmail.com>
2022-05-09 23:05:56 -0300

    ...

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -3845,6 +3845,8 @@ class Principal extends BaseController
         $Dt_IniVigencia = $this->input->post('Dt_IniVigencia');
         $Dt_FimVigencia = $this->input->post('Dt_FimVigencia');
         $Tp_Ativo = $this->input->post('Tp_Ativo');
+        $Dt_Ativo = null;
+        $Dt_Inativo = null;
 
         foreach ($this->PrincipalModel->carregaInfoRegraGruPro($IdRegraGruPro) as $data){
             $Tp_Ativo_Atual = ($data->Tp_Ativo);
@@ -3862,13 +3864,12 @@ class Principal extends BaseController
         }
 
 
-
         $infoRegraGruPro = array('TbGrupoPro_CodGrupo'=>$TbGrupoPro_CodGrupo, 'TbEmpresa_Id_Empresa'=>$this->session->userdata('IdEmpresa'),
             'TbRegra_Id_Regra'=> $TbRegra_Id_Regra, 'TbFaturamento_Id_Faturamento'=> $TbFaturamento_Id_Faturamento,'Perc_Pago'=>$Perc_Pago,
             'Dt_IniVigencia'=>$Dt_IniVigencia, 'Dt_FimVigencia'=>$Dt_FimVigencia, 'CriadoPor'=>$this->vendorId,
             'AtualizadoPor'=>$this->vendorId,'Tp_GrupoPro'=>$Tp_GrupoPro, 'Tp_Ativo'=>$Tp_Ativo, 'Dt_Ativo'=>$Dt_Ativo);
 
-        var_dump($IdRegraGruPro);exit;
+        var_dump($infoRegraGruPro);exit;
 
         $resultado = $this->PrincipalModel->editaRegraGruPro($infoRegraGruPro,$IdRegraGruPro);
 
