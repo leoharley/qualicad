5077945746a53eec3ccf808f71d3ecf1906301e3
Leonardo <leoharleygoncalves@gmail.com>
2022-05-10 20:12:50 -0300

    ...

--- application/models/ExportacaoModel.php
+++ application/models/ExportacaoModel.php
@@ -5,7 +5,9 @@ class ExportacaoModel extends CI_Model
 
     function exportaFatItem_Tudo()
     {
-        $this->db->select('*');
+        $this->db->select('Id_FatItem,CodFatItem,TbFaturamento_Id_Faturamento,Ds_FatItem,Dt_IniVigencia,
+        Dt_FimVigencia,Vl_Honorário,Vl_Operacional,Vl_Total,Vl_Filme,Cd_PorteMedico,Cd_TUSS,Cd_TISS,Qt_Embalagem,
+        Ds_Unidade');
         $this->db->from('TbFatItem as FatItem');
         $this->db->where('FatItem.Deletado !=', 'S');
         $this->db->where('FatItem.Tp_Ativo', 'S');
