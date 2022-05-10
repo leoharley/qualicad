d16cdc966e4b945b02ddeed3c06673d6907b159d
Leonardo <leoharleygoncalves@gmail.com>
2022-05-10 14:23:36 -0300

    ...

--- application/views/qualicad/principal/c_principalRegra.php
+++ application/views/qualicad/principal/c_principalRegra.php
@@ -182,7 +182,7 @@ if(!empty($infoRegra))
 
                                             <td style="width:10%">
                                                 <select class="form-control" name="Tp_GrupoPro" ng-model="teste" ng-disabled="teste2!=='SELECIONE'">
-                                                    <option value="SELECIONE"  selected>SELECIONE</option>
+                                                    <option value="SELECIONE" ng-selected="true">SELECIONE</option>
                                                     <option value="MD">MD</option>
                                                     <option value="MT">MT</option>
                                                     <option value="OP">OP</option>
@@ -194,7 +194,7 @@ if(!empty($infoRegra))
 
                                             <td>
                                                 <select class="form-control" name="TbGrupoPro_CodGrupo" ng-model="teste2" ng-disabled="teste!=='SELECIONE'">
-                                                    <option value="SELECIONE"  selected>SELECIONE</option>
+                                                    <option value="SELECIONE" ng-selected="true">SELECIONE</option>
                                                     <?php
                                                     if(!empty($infoGrupoPro))
                                                     {
