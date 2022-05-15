41db805e9237edd295dd42f62b8d0cb913919066
Leonardo <leoharleygoncalves@gmail.com>
2022-05-14 21:20:19 -0300

    ...

--- application/views/qualicad/cadastro/l_cadastroUsuario.php
+++ application/views/qualicad/cadastro/l_cadastroUsuario.php
@@ -55,7 +55,7 @@
                     <th>Data inativo</th> -->
                     <?php echo 'usuário logado: ' .$this->session->userdata('userId');?>
                     <?php echo 'criado por:' .$registrosUsuarios[0]->CriadoPor; ?>
-                    <th <?php if (($this->session->userdata('userId')) != ($registrosUsuarios[0]->CriadoPor)) { echo 'style=\"display:none\"';}?>>Ações</th>
+                    <th <?php if (($this->session->userdata('userId')) !== ($registrosUsuarios[0]->CriadoPor)) { echo 'style=\"display:none\"';}?>>Ações</th>
                   </tr>
                 </thead>
                 <tbody>
