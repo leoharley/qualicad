58ec26732acaa5bb2975f5534173d258a9410a28
Leonardo <leoharleygoncalves@gmail.com>
2022-05-18 23:50:17 -0300

    ...

--- application/views/qualicad/principal/c_principalExcecaoValores.php
+++ application/views/qualicad/principal/c_principalExcecaoValores.php
@@ -260,20 +260,27 @@ if(!empty($infoExcecaoValores))
         $(":input").inputmask();
         $('.valor').maskMoney();
 
-        $('#itemName').select2({
-        placeholder: '--- Select Item ---',
-        ajax: {
-          url: '/buscaProFat',
-          dataType: 'json',
-          delay: 250,
-          processResults: function (data) {
-            return {
-              results: data
-            };
-          },
-          cache: true
-        }
-      });
+        $("#itemName").select2({
+                placeholder: "Buscar",
+                minimumInputLength: 3,
+                ajax: {
+                    url:"/buscaProFat",
+                    dataType: 'json',
+                    quietMillis: 100,
+                    data: function (term, page) {
+                        return {
+                            q: term, //search term
+                            page_limit: 10, // page size
+                            page: page
+                        };
+                    },
+                    results: function (data, page) {
+                        var more = (page * 10) < data.total;
+                        return {results: data.results, more: more};
+                    }
+                },
+                escapeMarkup: function (m) { return m; }
+            });
 
     });
     shortcut.add("ctrl+l", function() {
