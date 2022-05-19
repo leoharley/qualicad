b6b3e9d42f4a45c5c9f86b1abd1587484176356a
Leonardo <leoharleygoncalves@gmail.com>
2022-05-18 23:05:47 -0300

    ...

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -22,10 +22,7 @@
   <!-- AdminLTE Skins. Choose a skin from the css/skins 
          folder instead of downloading all of them to reduce the load. -->
   <link href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js" defer></script>
-
+  
   <style>
     .error {
       color: red;
