f128b1de0e9b4587d15e0bda63230522c0b86e29
Leonardo <leoharleygoncalves@gmail.com>
2022-05-18 23:38:19 -0300

    ...

--- application/models/PrincipalModel.php
+++ application/models/PrincipalModel.php
@@ -1214,7 +1214,8 @@ function buscaProFat($Ds_ProFat)
     {
         $this->db->select('ProFat.CodProFat as id, ProFat.Ds_ProFat as text');
         $this->db->from('TbProFat as ProFat');
-        $this->db->where("(ProFat.Ds_ProFat LIKE '%".$Ds_ProFat."%')");
+        $likeCriteria = "(ProFat.Ds_ProFat LIKE '%".$Ds_ProFat."%')";
+        $this->db->where($likeCriteria);
         $query = $this->db->get();
 
         return $query->result();
