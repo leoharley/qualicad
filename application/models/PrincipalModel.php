acb0988806f54a3603bdcf317b38dcd54c176f1f
Leonardo <leoharleygoncalves@gmail.com>
2022-05-18 23:42:20 -0300

    ...

--- application/models/PrincipalModel.php
+++ application/models/PrincipalModel.php
@@ -1212,7 +1212,7 @@ function carregaInfoExcValoresEmpresa($idEmpresa)
 
 function buscaProFat($Ds_ProFat)
     {
-        var_dump($Ds_ProFat[0]->q);exit;
+        var_dump($Ds_ProFat);exit;
         $this->db->select('ProFat.CodProFat as id, ProFat.Ds_ProFat as text');
         $this->db->from('TbProFat as ProFat');
         $this->db->like('ProFat.Ds_ProFat', $Ds_ProFat[0]->text);
