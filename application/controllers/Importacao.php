4983cb13e9f28c4ed92e95c50570eefc869b7ad6
Leonardo <leoharleygoncalves@gmail.com>
2022-05-20 09:19:46 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -748,7 +748,6 @@ class Importacao extends BaseController
                             }
 
                             $memData += array(
-                                'TbUsuEmp_Id_UsuEmp' => $this->session->userdata('IdUsuEmp'),
                                 'TbEmpresa_Id_Empresa'=>$this->session->userdata('IdEmpresa'),
                                 'Tp_Ativo'=> 'S');
 
