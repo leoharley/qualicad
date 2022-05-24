8c0353fcc70f38cc15e45dfe62a86a4867d1b785
Leonardo <leoharleygoncalves@gmail.com>
2022-05-24 20:59:18 -0300

    ...

--- system/database/DB_query_builder.php
+++ system/database/DB_query_builder.php
@@ -1661,10 +1661,9 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 			array_values($this->qb_set)
 		);
 
-		var_dump($table);exit;
-		if ($table == 'Tb_Producao') {
+	/*	if ($table == 'Tb_Producao') {
 		var_dump($sql);exit;
-		}
+		} */
 
 		$this->_reset_write();
 		return $this->query($sql);
