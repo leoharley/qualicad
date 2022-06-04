ba00f3a647d2a1a74a259e9b9af748cf1d46a402
Leonardo <leoharleygoncalves@gmail.com>
2022-06-03 23:31:16 -0300

    ....

--- application/models/ExportacaoModel.php
+++ application/models/ExportacaoModel.php
@@ -474,6 +474,7 @@ where TbConvenio.tbempresa_id_empresa = '.$idEmpresa.'  -- selecionar empresa
       LEFT OUTER JOIN Tb_FracaoSimproBra ON (Tb_FracaoSimproBra.TbEmpresa_Id_Empresa = EXV.tbempresa_id_empresa and Tb_FracaoSimproBra.TbFatItem_Id_FatItem =EXV.TbFaturamento_Id_Faturamento and Tb_FracaoSimproBra.CD_TISS = EXV.Cd_TISS)
        Left outer join Tb_RegraProibicao on ( Tb_RegraProibicao.TbEmpresa_Id_Empresa =  EXV.tbempresa_id_empresa and Tb_RegraProibicao.TbPlano_Id_Plano = EXV.Id_Plano and Tb_RegraProibicao.TbGrupoPro_CodGrupo = EXV.CodGrupo )) CON_CONV_FINAL";
 
+var_dump($sql);exit;
     $query = $this->db->query($sql);    
     return $query->result();
     }
