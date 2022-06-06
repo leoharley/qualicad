d00337873a4ae244b2e4be72052444ac4d1b8dc6
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-06-06 15:01:26 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -507,10 +507,10 @@ class Exportacao extends BaseController
         }
 
 
-        $this->session->set_flashdata('offset', $offset + 10000);
-        $this->session->set_flashdata('limit', $limit + 10000);
+        $offsetSession = $this->session->set_flashdata('offset', $offset + 10000);
+        $limitSession = $this->session->set_flashdata('limit', $limit + 10000);
 
-        $successMsg = 'MSG TEMPORÁRIA: TABELA TMP_CONVENIO ATUALIZADA COM SUCESSO! Inseridos ('.$insertCountConvenio.') | Não inseridos ('.$notAddCountConvenio.')<br/>
+        $successMsg = 'MSG TEMPORÁRIA: TABELA TMP_CONVENIO ATUALIZADA COM SUCESSO! Limit ('.$limitSession.') | Offset ('.$offsetSession.') | Inseridos ('.$insertCountConvenio.') | Não inseridos ('.$notAddCountConvenio.')<br/>
                         MSG TEMPORÁRIA: TABELA TMP_CONTRATO ATUALIZADA COM SUCESSO! Inseridos ('.$insertCountContrato.') | Não inseridos ('.$notAddCountContrato.')';
         
         $this->session->set_flashdata('success', $successMsg);
