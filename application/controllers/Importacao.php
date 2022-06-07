46382c05ee92ea003f3ed328d649ac70b5b69d7a
Leonardo <leoharleygoncalves@gmail.com>
2022-06-07 20:03:05 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -2316,11 +2316,11 @@ class Importacao extends BaseController
 
     function valor($valor)
     {
-        $cleanString = preg_replace('/([^0-9\.,])/i', '', $valor);
+    /*    $cleanString = preg_replace('/([^0-9\.,])/i', '', $valor);
         $onlyNumbersString = preg_replace('/([^0-9])/i', '', $valor);
-     //   $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+        $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
         $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $onlyNumbersString);
-        $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+        $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot); */
         return (float) str_replace(',', '.', floatval($valor));
     //    return (float) str_replace(',', '.', $removedThousandSeparator);
     }
