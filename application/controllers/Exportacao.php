09844d8d7ac2e3413a26f087fc871b622e1e1688
Leonardo <leoharleygoncalves@gmail.com>
2022-06-14 00:54:32 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -441,7 +441,7 @@ class Exportacao extends BaseController
 
     function exportaBI()
     {
-        exportaTbBI();
+        $this->exportaTbBI();
         $idConvenio = $this->input->post('TbConvenio_Id_Convenio');
         $idEmpresa = $this->input->post('Id_Empresa');
 
