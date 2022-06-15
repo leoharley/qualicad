f136db30d86169828b0651d34c9c2de881630019
Leonardo <leoharleygoncalves@gmail.com>
2022-06-14 23:00:00 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -453,6 +453,8 @@ class Exportacao extends BaseController
 
     function exportaBI()
     {
+        redirect('exportacaoTbBI_progresso');
+        
         $idConvenio = $this->input->post('TbConvenio_Id_Convenio');
         $idEmpresa = $this->input->post('Id_Empresa');
 
