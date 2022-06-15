bb46f832bed3ad74f5ca73c9b1c9f6155c1977bc
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-06-15 14:54:55 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -613,10 +613,13 @@ class Exportacao extends BaseController
         unset($memData);
         $memData = array();
 
+        var_dump($idEmpresa);
+        var_dump($this->ExportacaoModel->consultaCodERPEmpresa($idEmpresa)[0]->Cd_EmpresaERP);
+        var_dump($idConvenio);
+        exit;
+
         $consultaTbBI = $this->ExportacaoModel->consultaTbBI($idEmpresa,$this->ExportacaoModel->consultaCodERPEmpresa($idEmpresa)[0]->Cd_EmpresaERP,$idConvenio,40000,$o);
-
-
-        var_dump($consultaTbBI);exit;
+        
         if (!empty($consultaTbBI)) {
         foreach($consultaTbBI as $row) {
                 foreach($row as $key => $value) {
