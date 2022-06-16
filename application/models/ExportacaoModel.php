6b55af3159d818efadde5a4f5a3239db979d513d
Leonardo <leoharleygoncalves@gmail.com>
2022-06-16 14:10:14 -0300

    ...

--- application/models/ExportacaoModel.php
+++ application/models/ExportacaoModel.php
@@ -716,6 +716,8 @@ where TbConvenio.tbempresa_id_empresa = $idEmpresa
         $sql = "KILL $id";
         $query = $this->db->query($sql);
 
+        var_dump($sql);exit;
+
         if($query) 
         return true;
         else
