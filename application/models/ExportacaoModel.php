4e5c2ea956b36f3f6fe0910d3694781ff417c887
Leonardo <leoharleygoncalves@gmail.com>
2022-06-16 23:44:25 -0300

    ...

--- application/models/ExportacaoModel.php
+++ application/models/ExportacaoModel.php
@@ -979,7 +979,6 @@ FROM
 
     function consultaCodERPEmpresa($IdEmpresa)
     {
-        var_dump($IdEmpresa);exit;
         $this->db->select('empresa.Cd_EmpresaERP');
         $this->db->from('TbEmpresa as empresa');
         $this->db->where('empresa.Id_Empresa', $IdEmpresa);
