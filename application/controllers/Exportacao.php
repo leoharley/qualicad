b02967bbd2e846fb3819782a4867661b1cc391f5
Leonardo <leoharleygoncalves@gmail.com>
2022-06-16 23:23:40 -0300

    ...

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -615,7 +615,7 @@ class Exportacao extends BaseController
         unset($memData);
         $memData = array();
 
-        $consultaTbBI = $this->ExportacaoModel->consultaTbBI($idEmpresa,$this->ExportacaoModel->consultaCodERPEmpresa($idEmpresa)[0]['Cd_EmpresaERP'],$idConvenio,25000,$o);
+        $consultaTbBI = $this->ExportacaoModel->consultaTbBI($idEmpresa,$this->ExportacaoModel->consultaCodERPEmpresa($idEmpresa)[0]->Cd_EmpresaERP,$idConvenio,25000,$o);
 
         if (!empty($consultaTbBI)) {
         foreach($consultaTbBI as $row) {
