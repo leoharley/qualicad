825036dc86d2e54d435e112bad86e47492da413a
Leonardo <leoharleygoncalves@gmail.com>
2022-06-18 15:41:22 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -493,6 +493,8 @@ class Importacao extends BaseController
                             $memData += array(                                
                                 'TbEmpresa_Id_Empresa'=>$this->session->userdata('IdEmpresa'),
                                 'Tp_Ativo'=> 'S');
+
+                            var_dump($memData);exit;    
                                 
                             $insert = 0;
                             // ***** VERIFICAÇÕES DE DUPLICIDADE NA ADIÇÃO *****
