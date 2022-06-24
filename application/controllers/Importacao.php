db7d1811b17601105650119fbd7efc09a3f8df28
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-06-24 16:52:20 -0300

    ...

--- application/controllers/Importacao.php
+++ application/controllers/Importacao.php
@@ -2359,6 +2359,8 @@ class Importacao extends BaseController
                     // Parse data from CSV file
                     $csvData = $this->csvreader->parse_csv($_FILES['file']['tmp_name'], 'simpro');
 
+                    var_dump($csvData);exit;
+
                     // Insert/update CSV data into database
                     if(!empty($csvData)){
                         foreach($csvData as $row) {
