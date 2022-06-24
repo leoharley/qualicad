047b6803a2dd351348131d0e3e61b59afa403187
Leonardo <leoharleygoncalves@gmail.com>
2022-06-24 08:48:32 -0300

    ...

---
+++ application/views/qualicad/importacao/importacaoSimproMae.php
@@ -0,0 +1,114 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            <i class="fa fa-users"></i> Importa Simpro (Carga mãe)
+            <small>Importação de tabela mãe</small>
+        </h1>
+    </section>
+
+<style>
+  table {
+    border-color: #808080!important;
+  }
+  th {
+    border-color: #808080!important;
+    color: black;
+    background-color: #d0d0d0;
+    }
+  td {
+    border-color: #808080!important;
+    color: black;
+    }
+    #importFrm {
+        margin-bottom: 20px;
+    } 
+  </style>   
+
+    <section class="content">
+
+
+<div class="container">
+    <?php
+    $this->load->helper('form');
+    $error = $this->session->flashdata('error');
+    if($error)
+    {
+        ?>
+        <div class="alert alert-danger alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <?php echo $this->session->flashdata('error'); ?>
+        </div>
+    <?php } ?>
+    <?php
+    $success = $this->session->flashdata('success');
+    if($success)
+    {
+        ?>
+        <div class="alert alert-success alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <?php echo $this->session->flashdata('success'); ?>
+            <br/>
+            <?php
+            $errosDeChaveMsg = $this->session->flashdata('errosDeChaveMsg');
+            if ($errosDeChaveMsg != '') echo 'VERIFICAR AS LINHAS (não inseridas): '. $errosDeChaveMsg; ?>
+        </div>
+    <?php } ?>
+
+    <div class="row">
+		
+        <!-- File upload form -->
+        <div class="col-md-12" id="importFrm">
+            <form action="<?php echo base_url() ?>importaSimproMae" method="post" enctype="multipart/form-data">
+                <br/>
+                <input type="file" name="file"/>
+                <br/>
+                <input type="submit" class="btn btn-primary" name="importSubmit" id="importSubmit" value="ATUALIZAR">
+                
+                <div id="loader" style="display:none;margin-top:30px">
+                <span><strong>O arquivo está sendo carregado, aguarde...    </strong></span>
+                <img src="<?php echo base_url(); ?>assets/images/loading.gif" style="width:100px;height:auto">
+                </div>
+
+            </form>
+        </div>
+
+        <br/>
+
+    <!--    <a class="btn btn-primary" href="<?php //echo base_url(). 'exportaProducao/'.$this->session->flashdata('num_linhas_importadas'); ?>" <?php if ($this->session->flashdata('num_linhas_importadas') == null) {echo 'disabled'; echo ' onclick=\'return false;\''; } ?>>
+            <i class="fa fa-upload"></i> Exportar importação atual</a>
+        <a class="btn btn-primary" href="<?php //echo base_url(); ?>exportaProducao/0">
+            <i class="fa fa-upload"></i> Exportar todos registros</a> -->  
+    </div>
+</div>
+
+</section>
+</div>
+
+<script>
+function formToggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
+$(document).ready(function () {
+        $('#importSubmit').attr('disabled', true);
+        $('input:file').change(
+            function () {
+                if ($(this).val()) {
+                    $('#importSubmit').removeAttr('disabled');
+                }
+                else {
+                    $('#importSubmit').attr('disabled', true);
+                }
+            });
+
+            $('#importSubmit').click(
+            function () {
+                $('#loader').show();
+            });
+    });
+</script>
