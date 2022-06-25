e1d5f3edb2b83b4ccc540d29c3d30fa27267b622
Leonardo <leoharleygoncalves@gmail.com>
2022-06-24 22:10:43 -0300

    ....

--- application/libraries/CSVReader.php
+++ application/libraries/CSVReader.php
@@ -48,10 +48,16 @@ class CSVReader {
         $csvFile = fopen($filepath, 'r');
         
         // Get Fields and values
-        $this->fields = fgetcsv($csvFile, $this->max_row_size, $this->separator, $this->enclosure);
-        $linha = 'CD_USUARIO|CD_FRACAO|DESCRICAO|VIGENCIA|IDENTIF|PC_EM_FAB|PC_EM_VEN|PC_EM_USU|PC_FR_FAB|PC_FR_VEN|PC_FR_USU|TP_EMBAL|TP_FRACAO|QTDE_EMBAL|QTDE_FRAC|PERC_LUCR|TIP_ALT|FABRICA|CD_SIMPRO|CD_MERCADO|PERC_DESC|VLR_IPI|CD_REG_ANV|DT_REG_ANV|CD_BARRA|LISTA|HOSPITALAR|FRACIONAR|CD_TUSS|CD_CLASSIF|CD_REF_PRO|GENERICO|DIVERSOS';
-        $keys_values = explode('|', $linha);
-        $keys = $this->escape_string($keys_values);
+        if ($tpImportacao == 'simpro') {
+            $linha = 'CD_USUARIO|CD_FRACAO|DESCRICAO|VIGENCIA|IDENTIF|PC_EM_FAB|PC_EM_VEN|PC_EM_USU|PC_FR_FAB|PC_FR_VEN|PC_FR_USU|TP_EMBAL|TP_FRACAO|QTDE_EMBAL|QTDE_FRAC|PERC_LUCR|TIP_ALT|FABRICA|CD_SIMPRO|CD_MERCADO|PERC_DESC|VLR_IPI|CD_REG_ANV|DT_REG_ANV|CD_BARRA|LISTA|HOSPITALAR|FRACIONAR|CD_TUSS|CD_CLASSIF|CD_REF_PRO|GENERICO|DIVERSOS';
+            $keys_values = explode('|', $linha);
+            $keys = $this->escape_string($keys_values);
+        }
+        else if ($tpImportacao == null) {
+            $this->fields = fgetcsv($csvFile, $this->max_row_size, $this->separator, $this->enclosure);        
+            $keys_values = explode('|', $this->fields[0]);
+            $keys = $this->escape_string($keys_values);
+        }        
         
         // Store CSV data in an array
         $csvData = array();
@@ -61,7 +67,7 @@ class CSVReader {
             if($row != NULL){
 
             //    if ($tpImportacao == null) {
-                    $values = explode('|', $row[1]);
+                    $values = explode('|', $row[0]);
             //    }
             /*    else if ($tpImportacao == 'simpro') {
                     $linha = 'CD_USUARIO|CD_FRACAO|DESCRICAO|VIGENCIA|IDENTIF|PC_EM_FAB|PC_EM_VEN|PC_EM_USU|PC_FR_FAB|PC_FR_VEN|PC_FR_USU|TP_EMBAL|TP_FRACAO|QTDE_EMBAL|QTDE_FRAC|PERC_LUCR|TIP_ALT|FABRICA|CD_SIMPRO|CD_MERCADO|PERC_DESC|VLR_IPI|CD_REG_ANV|DT_REG_ANV|CD_BARRA|LISTA|HOSPITALAR|FRACIONAR|CD_TUSS|CD_CLASSIF|CD_REF_PRO|GENERICO|DIVERSOS';
