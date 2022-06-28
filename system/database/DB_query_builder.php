931c70e90a4c058497e5db6bcfe823dd0c828020
Leonardo Harley de Morais Gonçalves <leoharleygoncalves@gmail.com>
2022-06-28 10:59:02 -0300

    ....

--- system/database/DB_query_builder.php
+++ system/database/DB_query_builder.php
@@ -1661,10 +1661,10 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 			array_values($this->qb_set)
 		);
 
-	/*	if ($table == 'Tb_FracaoSimproBra') {
+		if ($table == 'TbProFat') {
 		var_dump($sql);
 		exit;
-		} */
+		}
 
 		$this->_reset_write();
 		return $this->query($sql);
