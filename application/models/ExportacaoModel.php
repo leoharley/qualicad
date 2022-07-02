899c968196c3083dfee73b0a8daa113f72f7620d
Leonardo <leoharleygoncalves@gmail.com>
2022-07-02 20:23:36 -0300

    ...

--- application/models/ExportacaoModel.php
+++ application/models/ExportacaoModel.php
@@ -217,7 +217,7 @@ class ExportacaoModel extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('TbBI as BI');
-        $this->db->where('BI.Tp_Ativo', 'S');
+    //    $this->db->where('BI.Tp_Ativo', 'S');
     //    $this->db->where('BI.TbEmpresa_Id_Empresa', $idEmpresa);
         $query = $this->db->get();
 
