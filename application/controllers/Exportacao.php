adbea5af0680d609a47fbf37f75cafcad7fc8000
Leonardo <leoharleygoncalves@gmail.com>
2022-07-02 19:04:44 -0300

    ....

--- application/controllers/Exportacao.php
+++ application/controllers/Exportacao.php
@@ -470,6 +470,8 @@ class Exportacao extends BaseController
             $this->exportaTbBI();
         }
 
+        var_dump(($this->ExportacaoModel->consultaCodERPEmpresa($idEmpresa))[0]->Cd_EmpresaERP);exit;
+
         $idConvenio = $this->input->post('TbConvenio_Id_Convenio');
         $idEmpresa = $this->input->post('Id_Empresa');
 
