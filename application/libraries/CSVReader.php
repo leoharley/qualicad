8b46c158a972a027550b6285c58f28f8d5ca4c68
Leonardo <leoharleygoncalves@gmail.com>
2022-07-04 18:26:47 -0300

    ....

--- application/libraries/CSVReader.php
+++ application/libraries/CSVReader.php
@@ -44,8 +44,7 @@ class CSVReader {
         } else {
             $separador = '|';
         }
-        
-        var_dump($separador);exit;
+
         // If file doesn't exist, return false
         if(!file_exists($filepath)){
             return FALSE;            
@@ -56,7 +55,7 @@ class CSVReader {
         
         // Get Fields and values
         if ($tpImportacao == 'simpro') {
-            $linha = 'CD_USUARIO|CD_FRACAO|DESCRICAO|VIGENCIA|IDENTIF|PC_EM_FAB|PC_EM_VEN|PC_EM_USU|PC_FR_FAB|PC_FR_VEN|PC_FR_USU|TP_EMBAL|TP_FRACAO|QTDE_EMBAL|QTDE_FRAC|PERC_LUCR|TIP_ALT|FABRICA|CD_SIMPRO|CD_MERCADO|PERC_DESC|VLR_IPI|CD_REG_ANV|DT_REG_ANV|CD_BARRA|LISTA|HOSPITALAR|FRACIONAR|CD_TUSS|CD_CLASSIF|CD_REF_PRO|GENERICO|DIVERSOS';
+            $linha = 'CD_USUARIO;CD_FRACAO;DESCRICAO;VIGENCIA;IDENTIF;PC_EM_FAB;PC_EM_VEN;PC_EM_USU;PC_FR_FAB;PC_FR_VEN;PC_FR_USU;TP_EMBAL;TP_FRACAO;QTDE_EMBAL;QTDE_FRAC;PERC_LUCR;TIP_ALT;FABRICA;CD_SIMPRO;CD_MERCADO;PERC_DESC;VLR_IPI;CD_REG_ANV;DT_REG_ANV;CD_BARRA;LISTA;HOSPITALAR;FRACIONAR;CD_TUSS;CD_CLASSIF;CD_REF_PRO;GENERICO;DIVERSOS';
             $keys_values = explode($separador, $linha);
             $keys = $this->escape_string($keys_values);
         }
